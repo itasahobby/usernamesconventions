85181369d7f07ec6635d33488d985034b5c2a830
itasahobby <itasahobby@gmail.com>
2020-05-16 15:46:49 +0200

    Improved parameter parsing

--- usernameconv.py
+++ usernameconv.py
@@ -3,12 +3,13 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Generates a wordlist of usernames according to different conventions for the given name and surname")
-    parser.add_argument("-i","--input-file",action="store", dest="input_file",default="sampledata.json", help="Input json file")
-    parser.add_argument("-o","--output-file",action="store", dest="output_file",default="output", help="Output json file")
     parser.add_argument("-j","--output-json",action="store_true", dest="json", help="The output is generated as a json file instead of a wordlist")
-    parser.add_argument("-s","--substitute-conventions",action="store", dest="conventions_subs", help="Specify a file with custom conventions and substitute them all", metavar="CONVENTIONS")    
-    parser.add_argument("-a","--add-conventions",action="store", dest="conventions_add", help="Specify a file with custom conventions and add them to default ones", metavar="CONVENTIONS")
+    convention_parser = parser.add_mutually_exclusive_group()
+    convention_parser.add_argument("-s","--substitute-conventions",action="store", dest="conventions_subs", help="Specify a file with custom conventions and substitute them all", metavar="CONVENTIONS")    
+    convention_parser.add_argument("-a","--add-conventions",action="store", dest="conventions_add", help="Specify a file with custom conventions and add them to default ones", metavar="CONVENTIONS")
     parser.add_argument("-S","--set-separator",action="store",dest="separator",default="&", help="Specify a custom separator used for conventions")
+    parser.add_argument("-i","--input-file",action="store", dest="input_file",default="sampledata.json", help="Input json file", required=True)
+    parser.add_argument("-o","--output-file",action="store", dest="output_file",default="output", help="Output json file", required=True)
     return parser
 
 def get_conventions(separator):
@@ -58,10 +59,11 @@ def write_list(filedescr,datalist):
 def main():
     parser = get_parser()
     args = parser.parse_args()
+    conventions = get_conventions(args.separator)
     if(args.conventions_subs):
         conventions = read_conventions(args.conventions_subs)
     elif args.conventions_add:
-        conventions = read_conventions(args.conventions_add) + get_conventions(args.separator)
+        conventions = read_conventions(args.conventions_add) + conventions
     with open(args.input_file) as input_file:
         data = json.load(input_file)
         with open(args.output_file, "w") as output_file:
